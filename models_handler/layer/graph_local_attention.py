gradient flow fix, inplace operation on the tensor x inside the SuperBlock layer "x[:, 1:, :] = ... " was casuing issue during the gradient computation

models_handler/layer/graph_local_attention.py
@@ -32,7 +32,7 @@ class GraphLocalAttention(nn.Module):
         attn_mask: Tensor | None = None, 
         bpt_partitions: Tensor | None = None
     ) -> Tensor:
-        patches: Tensor = x[:, 1:, :]
+        patches: Tensor = x
 
         graph_genout: GraphGenout = generate_connection(
             patches_emb=patches,

models_handler/layer/super_block.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 from timm.models.vision_transformer import Block
 from timm.layers import (LayerNorm, LayerType, Mlp, Attention)
 from torch import Tensor
+import torch
 import torch.nn as nn
 from typing import Optional, Type
 
@@ -71,16 +72,27 @@ class SuperBlock(Block):
     
 
     def forward(
-        self, x: Tensor, 
+        self, 
+        x: Tensor, 
         attn_mask: Tensor | None = None, 
         bpt_partitions: Tensor | None = None
     ) -> Tensor:
-        x[:, 1:, :] = self.norm0(x[:, 1:, :])
+        norm0_x: Tensor = self.norm0(x[:, 1:, :])
         local_out: Tensor = self.local_attention(
-            x=x,
+            x=norm0_x,
             attn_mask=attn_mask,
             bpt_partitions=bpt_partitions
         )
-        x[:, 1:, :] = x[:, 1:, :] + local_out
+        local_attention: Tensor = x[:, 1:, :] + local_out
+        pre_block_out: Tensor = torch.cat(
+            [
+                x[:, 1, :].unsqueeze(dim=1), 
+                local_attention
+            ], 
+            dim=1
+        )
 
-        return super().forward(x=x, attn_mask=attn_mask)
+        return super().forward(
+            x=pre_block_out, 
+            attn_mask=attn_mask
+        )

models_handler/transformer/graph_vit.py
@@ -64,11 +64,12 @@ class GraphVit(VitClassifier):
         self.backbone = GraphVisionTransformer.build_from_vision_transformer(
             vit_model=self.backbone,    
             gnn_type=self.hparams.gnn_type,
-            gnn_num_layer=self.hparams.gnn_num_layer,
-            global_pool=HeadType[self.hparams.head_type].value
+            gnn_num_layer=self.hparams.gnn_num_layer
         )
 
     def on_train_start(self) -> None:
+        torch.autograd.set_detect_anomaly(True)
+
         # TODO see if these two are necessary
         # technically they shouldn't
         self.backbone.head.requires_grad = True
@@ -215,7 +216,12 @@ class GraphVit(VitClassifier):
         batch: GraphVitInput, 
         step_type: str = "train"
     ) -> CleopatraOut:
-        img, label, attention_mask, bpt_info, _ = batch
+        
+        if isinstance(batch, GraphVitInput):
+            img, label, attention_mask, bpt_info, _ = batch
+        else:
+            img, label, attention_mask, _ = batch
+            bpt_info = None
 
         weights = self.loss_weights if step_type == "train" else torch.ones_like(self.loss_weights) 
 